9947e413e2d0383942f6e34760bbdda304e0bafc
Gustavo henrique dos santos teixeira <160744948+xGustafsv@users.noreply.github.com>
2024-09-17 12:10:16 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,15 @@
+// tipo Number
+
+const numero01 = 3
+const numero02 = 10
+const numero03 = 7
+
+const soma = numero01 + numero02
+const subtracao  = numero02 - numero03
+const multiplicacao = numero02 * numero01
+const divisao = numero03 / numero03 
+
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
